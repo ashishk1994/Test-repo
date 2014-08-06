be941178985e83c4b86905d599cef9229c08e336
Ashish Kumar <coolyashish@gmail.com>
2014-08-06 10:46:09 +0530

    MY

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('app', ['app.controllers','app.services'])
+angular.module('app', ['app.controllers','app.services','app.service2','app.controller2','app.service3'])
   // config(['$routeProvider', function($routeProvider) {
   //   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   //   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
--- js/services.js
+++ js/services.js
@@ -3,11 +3,20 @@ angular.module('app.services', ['ngResource'])
     
 
    .factory('AngularIssues', function($resource){
-	     return $resource('../test.json',{},{
-         query: {
-                    isArray: true,
-                    method:'GET'
-                }}
-       )
+	     return $resource('mocks/data.json/:id',{id:'@m_id'});
+
    })
+angular.module('app.service2', ['ngResource'])
+
+   .factory('AngularIssue', function($resource){
+	     return $resource('mocks/single.json/:id',{id:'@m_id'});
+
+   })
+angular.module('app.service3', ['ngResource'])
+
+   .factory('FormService', function($resource){
+	     return $resource('mocks/form.json/:id',{id:'@m_id'});
+
+   })
+
 //   .value('version', '0.1');
